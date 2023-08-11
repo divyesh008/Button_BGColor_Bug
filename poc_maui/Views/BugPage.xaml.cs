ae603f151c08617e68c783da25840df15f8685fb
Divyesh Bhatt <divyesh.bhatt23@gmail.com>
2023-08-11 12:43:14 +0530

    initial commit

---
+++ poc_maui/Views/BugPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using poc_maui.ViewModels;
+
+namespace poc_maui.Views;
+
+public partial class BugPage : ContentPage
+{
+	public BugPage()
+	{
+		InitializeComponent();
+	}
+
+    void CheckBox_CheckedChanged(System.Object sender, Microsoft.Maui.Controls.CheckedChangedEventArgs e)
+    {
+        var _vm = (BindingContext as BugPageViewModel);
+        _vm.HandleViolationCodeSelection();
+    }
+}
